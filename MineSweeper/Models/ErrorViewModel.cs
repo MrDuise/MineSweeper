fcda4f4ddaaa00786b80183dd7b614fab8501bfe
MrDuise <53163559+MrDuise@users.noreply.github.com>
2022-01-21 18:11:58 -0700

    Add project files.

---
+++ MineSweeper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MineSweeper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
