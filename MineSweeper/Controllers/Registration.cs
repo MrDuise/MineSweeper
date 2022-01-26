301b13d7867e439db8c910b83bca07648fcdf16d
MrDuise <53163559+MrDuise@users.noreply.github.com>
2022-01-25 22:43:06 -0700

    Added views for register success and register failure. Plus a register view

---
+++ MineSweeper/Controllers/Registration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MineSweeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Controllers
+{
+    public class Registration : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessRegister(UserModel user)
+        {
+            //SecurityService securityService = new SecurityService();
+            if ("blank" == "blank")
+            {
+                return View("RegisterSuccess", user);
+            }
+            else
+            {
+                return View("RegisterFailure", user);
+            }
+        }
+    }
+}
