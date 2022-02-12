cde4fcac07cb779f72fb8458a0b81b701479a85d
Michael Duisenberg <mrduise@gmail.com>
2022-02-12 15:42:43 -0700

    created first version of gameboard

---
+++ MineSweeper/Controllers/GameBoardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MineSweeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Controllers
+{
+    public class GameBoardController : Controller
+    {
+        static List<ButtonModel> buttons = new List<ButtonModel>();
+        Random random = new Random();
+        const int GRID_SIZE = 25;
+        public IActionResult Index()
+        {
+            if (buttons.Count < GRID_SIZE)
+            {
+                for (int i = 0; i < GRID_SIZE; i++)
+                {
+                    buttons.Add(new ButtonModel(i));
+                }
+            }
+
+
+
+            return View("Index", buttons);
+        }
+    }
+}
---
+++ MineSweeper/Models/ButtonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Models
+{
+    public class ButtonModel
+    {
+       public int id { get; set; }
+        public bool visited { get; set; }
+        public bool live { get; set; }
+        public int liveNeighbors { get; set; }
+        public bool flag { get; set; }
+
+        public string buttonFile { get; set; }
+
+        /// <summary>
+        /// Constructor. Sets properities to default values
+        /// </summary>
+        public ButtonModel(int ID)
+        {
+            id = ID;
+            visited = false;
+            live = false;
+            liveNeighbors = 0;
+
+            
+        }
+
+    }
+}
