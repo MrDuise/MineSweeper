38e935d2fac82c7bc0da28c33e2d3f883606fb30
Zac Almas <zalmas@my.gcu.edu>
2022-03-15 00:57:14 -0600

    Right Click (Not Fully Functional) (#11)
    * Right Click (Not Fully Functional)
    
    Right click now disables/enables buttons and displays a flag, the flag only displays after another button is clicked however... Soon to be fixed
    
    * Flag Click
    
    Right click works much better now. Small bug where the flag placement is off but other than that it works fine
    
    Co-authored-by: Zac Almas <zacalmas@Zacs-MacBook-Pro.local>

--- MineSweeper/Controllers/GameBoardController.cs
+++ MineSweeper/Controllers/GameBoardController.cs
@@ -22,19 +22,46 @@ namespace MineSweeper.Controllers
                     buttons.Add(new ButtonModel(i));
                 }
             }
-            return View("Index", buttons);
+            return View(buttons);
         }
 
         public IActionResult HandleButtonClick(string buttonNumber)
         {
            
             int bn = int.Parse(buttonNumber);
+            if (buttons.ElementAt(bn).enabled == true)
+            {
+                buttons.ElementAt(bn).visited = true;
+            } 
 
-            buttons.ElementAt(bn).visited = true;
-
-            
-           
             return View("Index", buttons);
+        }
+
+        public IActionResult ShowOneButton(int buttonNumber)
+        {
+            return PartialView(buttons.ElementAt(buttonNumber));
+        }
+
+        public IActionResult RightClickShowButton(int buttonNumber)
+        {
+            if (buttons.ElementAt(buttonNumber).visited == false)
+            {
+                flagSwap(buttonNumber);
+            }
+            return PartialView("ShowOneButton", buttons.ElementAt(buttonNumber));
+        }
+
+        public void flagSwap(int buttonNumber)
+        {
+            if (buttons.ElementAt(buttonNumber).flag == false)
+            {
+                buttons.ElementAt(buttonNumber).flag = true;
+                buttons.ElementAt(buttonNumber).enabled = false;
+            } else
+            {
+                buttons.ElementAt(buttonNumber).flag = false;
+                buttons.ElementAt(buttonNumber).enabled = true;
+            }
         }
     }
 }
--- MineSweeper/wwwroot/js/site.js
+++ MineSweeper/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    console.log("Page is ready");
 
-// Write your JavaScript code.
+    $(document).bind("contextmenu", function (e) {
+        e.preventDefault();
+        console.log("Right click. Prevent context menu from showing.")
+    });
+
+    $(document).on("mousedown", ".game-button", function (event) {
+        switch (event.which) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                event.preventDefault();
+                var buttonNumber = $(this).val();
+                console.log("Button number " + buttonNumber + " was right clicked");
+                doButtonUpdate(buttonNumber, "/GameBoard/RightClickShowButton");
+                break;
+            default:
+                alert('Nothing');
+        }
+    });
+
+    function doButtonUpdate(buttonNumber, urlString) {
+        $.ajax({
+            datatype: "json",
+            method: 'POST',
+            url: urlString,
+            data: {
+                "buttonNumber": buttonNumber
+            },
+            success: function (data) {
+                console.log(data);
+                $("#" + buttonNumber).html(data);
+            }
+        });
+    }
+});
