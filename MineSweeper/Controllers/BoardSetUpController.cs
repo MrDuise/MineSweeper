143794699bf4fc3f71666ef8ae02bcb077f08642
Michael Duisenberg <53163559+MrDuise@users.noreply.github.com>
2022-04-02 22:05:47 -0600

    Overhauled game logic and how pages are generated (#15)
    Updated logic on both left click and right click

---
+++ MineSweeper/Controllers/BoardSetUpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Controllers
+{
+    public class BoardSetUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MineSweeper/Models/Cell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Models
+{
+    public class Cell
+    {
+        public int id { get; set; }
+        public int row { get; set; }
+        public int column { get; set; }
+        public bool visited { get; set; }
+        public bool live { get; set; }
+        public int liveNeighbors { get; set; }
+        public bool flag { get; set; }
+
+        public string buttonFile { get; set; }
+        public bool enabled { get; set; }
+
+        /// <summary>
+        /// Constructor. Sets properities to default values
+        /// </summary>
+        public Cell(int newID)
+        {
+            id = newID;
+            row = 0;
+            column = 0;
+            visited = false;
+            live = false;
+            liveNeighbors = 0;
+            flag = false;
+            enabled = true;
+        }
+    }
+}
